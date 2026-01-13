623201e2d983729a0fa2a0781accb1c89294f273
chihiro888 <b930208@gmail.com>
2026-01-13 18:44:31 +0900

    feat: update

--- user_data/strategies/BBMarginStrategy.py
+++ user_data/strategies/BBMarginStrategy.py
@@ -113,7 +113,7 @@ class BBMarginStrategy(IStrategy):
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # Bollinger Bands
-        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=10, stds=2)
         dataframe['bb_lowerband'] = bollinger['lower']
         dataframe['bb_middleband'] = bollinger['mid']
         dataframe['bb_upperband'] = bollinger['upper']
